30db67c9bc60a72e008575e7b848f9078b67a87e
Tracy Rothery <tracy.rothery@gmail.com>
2022-04-23 10:59:36 +0800

    update error

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 
 // Compnents 
@@ -10,9 +10,24 @@ import ProjectCard from "../components/ProjectCard/ProjectCard";
 
 
 function HomePage() {
+    // States
+    const [projectList, setProjectList] = useState([]);
+
+    //Actions & Helpers
+
+    useEffect(() => {
+        fetch(`${process.env.REACT_APP_API_URL}projects`)
+        .then((results) => {
+            return results.json();
+        })
+        .then((data) => {
+            setProjectList(data);
+        });
+    }, []);
+
     return (
         <div id="project-list">
-            {allProjects.map((projectData => {
+            {projectList.map((projectData => {
                 return<ProjectCard
                 key={'project-$[projectData.id}'}
                 projectData={projectData}
