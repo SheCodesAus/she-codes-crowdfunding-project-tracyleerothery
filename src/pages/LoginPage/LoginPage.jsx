e6ecf444d0c15ac66d6d8fcd33b42c5f1659a890
Tracy Rothery <tracy.rothery@gmail.com>
2022-05-05 21:39:25 +0800

    log in page

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 //Components
-import LoginForm from "../components/LoginForm/LoginForm";
+import LoginForm from "../../components/LoginForm/LoginForm";
 
 function LoginPage() {
     return <LoginForm />
