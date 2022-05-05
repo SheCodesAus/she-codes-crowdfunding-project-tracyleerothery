dbe46a04db36bcf38a9c726a1e5ae7e196dc96a9
Tracy Rothery <tracy.rothery@gmail.com>
2022-05-06 07:44:10 +0800

    Update to Fix Nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -27,31 +27,22 @@ function Nav() {
     }
 
     //check if user has token and change nav
-    const checkUser = (isBurgerMenu) => {
+    const checkUser = () => {
         // Get the user token. The !! ensure that the token "string" or undefined becomes true or false
         const isUserLoggedIn = !!window.localStorage.getItem("token");
         // console.log("isuserloggedin", isUserLoggedIn)
 
-        const className = "nav-links-mobile";
+        const className = "buttonclassname";
 
         const loginButton = <Button className={className} onClick={navigateToLogin}>Login</Button>;
-        const signOutButton = <Button className={className} onClick={handleSignOut}>Sign Out1</Button>;
-
-        const loginLink = <button className={className} onClick={handleSignOut}>Login</button>;
-        const signOutLink = <button className={className} onClick={handleSignOut}>Sign Out</button>
+        const signOutButton = <Button className={className} onClick={handleSignOut}>Sign Out1</Button>
 
         // This is a ternary operation
         //      Conditon ? True : false
         // Example: we check is the user logged in (yes => show sign out) (no => show login)
-        if (isBurgerMenu) {
-            return isUserLoggedIn 
-                ? signOutButton
-                : loginButton
-        } else {
-            return isUserLoggedIn 
-                ? signOutLink
-                : loginLink
-        }   
+        return isUserLoggedIn 
+            ? signOutButton
+            : loginButton
     }
 
     // Hamburger Handlers
@@ -79,18 +70,9 @@ function Nav() {
                     <Link className="button" to="/projects/create">Create a Project </Link>
                     <Link className="button" to="/users/register">Register </Link>
                    
-                
-                    {checkUser(false)}
-                    {/* {isLoggedIn
-                        ? <Link className="nav-links-mobile" to="/login">Login/Sign Up</Link>
-                        : ""
-                    } */}
+                    {checkUser()}
             </ul>
-            {checkUser(true)}
-            {/* {isLoggedIn
-                ? <Button className="nav-links-mobile"><Link to="/login">Login/Sign Up</Link></Button>
-                : ""
-            } */}
+        
         </nav>
     )
 };
