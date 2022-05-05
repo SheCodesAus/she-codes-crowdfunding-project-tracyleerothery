4ff1fe9e72f35130647925469ad61865ac800250
Tracy Rothery <tracy.rothery@gmail.com>
2022-05-05 22:52:12 +0800

    update home page with css

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,31 +1,65 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 
 // Components
 import ProjectCard from "../../components/ProjectCard/ProjectCard";
 
+// Styles
+import './HomePage.css';
+
+// Imports
+import { Link } from "react-router-dom";
+
 function HomePage() {
-  // State
-  const [projectList, setProjectList] = useState();
 
-  // Actions
-  useEffect(() => {
-    fetch(`${process.env.REACT_APP_API_URL}projects`)
-      .then((results) => {
-        return results.json();
-      })
-      .then((data) => {
-        setProjectList(data);
-      });
-  }, []);
+    // States
+    const [projectList, setProjectList] = useState([]);
 
-  if (!projectList) return <p>Loading projects...</p>;
+    // Action & Helpers
+    useEffect(() => {
+        fetch(`${process.env.REACT_APP_API_URL}projects`)
+            .then((results) => {
+                return results.json();
+            })
+            .then((data) => {
+                setProjectList(data);
+            });
+    }, []);
 
-  return (
-    <div id="project-list">
-      {projectList.map((projectData) => {
-        return <ProjectCard key={projectData.id} {...{ projectData }} />;
-      })}
+    return (
+        <div className="homepage-wrapper">
+          
+
+
+        <div id="welcome-text">    
+          
+    <div class="hero-image">
+    <div class="hero-text">
+      <h1>All dogs</h1><p>
+                    <button><Link to="/projects/create/">POST NEW PROJECT</Link></button>
+                </p>
+      <p>Helping every good boy and girl find a new home. Like Neopets but you get to help real animlas.</p>
     </div>
-  );
+</div> 
+                
+
+            </div>
+
+            <div id="project-list">
+                {projectList.map((projectData, key) => {
+                    return <ProjectCard 
+                        key={`project-${projectData.id}`} 
+                        projectData={projectData}
+                    />;
+                })}
+            </div>
+                
+                <br></br>
+            <div>
+                More to come...
+            </div>
+
+        </div>
+    );
 }
+
 export default HomePage;
