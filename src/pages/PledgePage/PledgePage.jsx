dab3a3e29e47022ca69e3e8241dd0315e476330d
Tracy Rothery <tracy.rothery@gmail.com>
2022-05-05 21:39:51 +0800

    pledge page

---
+++ src/pages/PledgePage/PledgePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+// Components
+import PledgeForm from "../../components/PledgeForm/PledgeForm";
+
+function PledgePage() {
+    return <PledgeForm />;
+}
+console.log();
+
+
+export default PledgePage;
