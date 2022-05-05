3e3b6a33821e38e4146fc607e61cd7d97e9c1b79
Tracy Rothery <tracy.rothery@gmail.com>
2022-05-05 21:40:15 +0800

    project form page

---
+++ src/pages/ProjectFormPage/ProjectFormPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+// Components
+import ProjectForm from "../../components/ProjectForm/ProjectForm";
+
+function ProjectFormPage() {
+    return <ProjectForm />;
+}
+
+export default ProjectFormPage;
