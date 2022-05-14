bf10858d0689a3d8774ef8046f01d6efcaef218d
Tracy Rothery <tracy.rothery@gmail.com>
2022-05-14 21:01:51 +0800

    css

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -46,7 +46,7 @@ function HomePage() {
                     <button><Link to="/projects/create/">SUPPORT A PAL</Link></button>
                     </p>
                     
-      <p className="homepage-text" >Helping every Pal get ready for a new home.</p> 
+      <h2>Helping every Pal get ready for a new home.</h2> 
       
     </div>
 </div> 
